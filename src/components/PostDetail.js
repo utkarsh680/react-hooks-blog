01ca5700f9939e6acf42d3828798a7cfcece0dd4
Utkarsh Singh <singhrishu680@gmail.com>
2023-07-17 21:45:55 +0530

    create postdetails

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { firestore } from "../firebase";
+
 function PostDetail() {
+
+  const[posts, setPosts] = useState([]);
+
+  useEffect(() => {
+
+    firestore
+    .collection('posts')
+    .onSnapshot(((snapshot) => {
+        const newPosts = snapshot.docs.map((doc) =>({
+            id: doc.id,
+            ...doc.data()
+        }))
+        setPosts(newPosts);
+    }
+    ))
+  }, []);
+
     return (
       <div className="">
-        PostDetail
+        <h1>Post Detail</h1>
+        {posts.map((post, index) => (
+            <div className="post" key={index}>
+              <h3>{post.title}</h3>
+              <p>{post.subTitle}</p>
+              <p>{post.content}</p>
+              
+             </div>
+          ))}
       </div>
     );
   }

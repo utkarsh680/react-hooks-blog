7d77d1770b861fc57932b0947f397b49bcf4c0b9
Utkarsh Singh <singhrishu680@gmail.com>
2023-07-18 16:44:18 +0530

    postdetail component change

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,34 +1,35 @@
 import { useEffect, useState } from "react";
 import { firestore } from "../firebase";
+import { useParams } from "react-router-dom";
 
 function PostDetail() {
 
   const[posts, setPosts] = useState([]);
+  const { postId } = useParams();
 
   useEffect(() => {
 
     firestore
     .collection('posts')
-    .onSnapshot(((snapshot) => {
-        const newPosts = snapshot.docs.map((doc) =>({
-            id: doc.id,
-            ...doc.data()
-        }))
-        setPosts(newPosts);
-    }
-    ))
+    .doc(postId)
+    .get()
+    .then((snapshot) => {
+        setPosts(snapshot.data());
+    })
+    .catch((error) => {
+        console.log(error);
+    })
   }, []);
 
     return (
       <div className="">
         <h1>Post Detail</h1>
-        {posts.map((post, index) => (
-            <div className="post" key={index}>
-              <h3>{post.title}</h3>
-              <p>{post.subTitle}</p>
-              <p>{post.content}</p> 
+        
+            <div className="post">
+              <h3>{posts.title}</h3>
+              <p>{posts.subTitle}</p>
+              <p>{posts.content}</p> 
              </div>
-          ))}
       </div>
     );
   }
